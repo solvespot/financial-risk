2594e33772c9a8dfa03ef460ba010935a29d53e1
solvespot <admin@solvespot.nl>
2020-10-04 20:41:33 +0200

    Update risk_1.py

--- risk_1.py
+++ risk_1.py
@@ -2,20 +2,25 @@ import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 import datetime as dt
+import matplotlib.pyplot as plt
+import seaborn as sbs
 
-start_date = dt.datetime(2017, 9, 26)
-end_date = dt.datetime(2020, 9, 26)
-tickers = ['es=f', 'zb=f', 'gc=f', 'cl=f']
+end_date = dt.datetime.today()
+start_date = end_date - dt.timedelta(weeks=156)
+tickers = ['es=f', 'ym=f', 'nq=f', 'zb=f', 'zf=f', 'gc=f', 'si=f', 'cl=f', 'ng=f']
+names = ['S&P', 'Dow', 'Nasdaq', 'US T 10y', 'US T 5Y', 'Gold', 'Silver', 'Crude', 'Nat Gas']
+mapping = {t: n for t, n in zip(tickers, names)}
 close_prices = pdr.DataReader(tickers, data_source='yahoo', start=start_date, end=end_date)['Close']
-print(close_prices.head(10))
+close_prices.rename(columns=mapping, inplace=True)
+print(close_prices[['Nasdaq', 'US T 10y']].head(5))
 
 close_prices.dropna(inplace=True)
 close_prices.sort_index(ascending=False, inplace=True)
-returns = (close_prices / close_prices.shift(-1) -1).dropna()
+returns = (close_prices / close_prices.shift(-1) - 1).dropna()
 print(returns.describe())
 
+sbs.heatmap(returns.corr(), cmap='RdYlGn', annot=True)
+plt.show()
 
-x=1
-
-x = np.DataF
-y = pd.DataFrame()
+a = np.ndarray()
+p = pd.DataFrame()
